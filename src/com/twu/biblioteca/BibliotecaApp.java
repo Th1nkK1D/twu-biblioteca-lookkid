94d197dd01a44f771755238bdfa09e158da054ff
Th1nkK1D <witheep@gmail.com>
2019-08-20 13:47:57 +0700

    Add list of book in main app

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,6 +1,9 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
+    private ArrayList<Book> ListOfBooks;
 
     public static void main(String[] args) {
         System.out.println(BibliotecaApp.getWelcomeMessage());
@@ -8,5 +11,13 @@ public class BibliotecaApp {
 
     public static String getWelcomeMessage() {
         return "Welcome to Biblioteca. Your one-stop-shop for great book titles on Bangalore!";
+    }
+
+    public BibliotecaApp(ArrayList<Book> ListOfBooks) {
+        this.ListOfBooks = ListOfBooks;
+    }
+
+    public ArrayList<Book> getListOfBooks() {
+        return this.ListOfBooks;
     }
 }
