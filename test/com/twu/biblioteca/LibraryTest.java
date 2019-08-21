a1031ed097a799d9a2ecc6735eca952c1786ee8b
Th1nkK1D <witheep@gmail.com>
2019-08-21 10:29:10 +0700

    Refactor: Move library initialize into setUp function in library test

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -10,71 +10,63 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
     private ArrayList<Book> listOfBooks = new ArrayList<Book>();
+    private Library library;
 
     @Before
     public void setUp() {
         this.listOfBooks.add(new Book("Java Basic", "Steve", 2008));
         this.listOfBooks.add(new Book("How to TDD", "James", 2012));
+
+        this.library = new Library(this.listOfBooks);
     }
 
     @Test
     public void libraryShouldBeAbleToProvideListOfAllBooks() {
-        Library library = new Library(this.listOfBooks);
-
-        assertThat(library.getListOfBooksName(), is("Java Basic\nHow to TDD\n"));
+        assertThat(this.library.getListOfBooksName(), is("Java Basic\nHow to TDD\n"));
     }
 
     @Test
     public void libraryShouldBeAbleToGiveAListOfAllBooksWithNameAuthorAndYear() {
-        Library library = new Library(this.listOfBooks);
-
         String expectedOutput =
             "| name                     | author              | Year Published  |\n" +
             "| -------------------------|---------------------|-----------------|\n" +
             "| Java Basic               | Steve               | 2008            |\n" +
             "| How to TDD               | James               | 2012            |\n";
 
-        assertThat(library.getListOfAllBooksWithNameAuthorAndYear(), is(expectedOutput));
+        assertThat(this.library.getListOfAllBooksWithNameAuthorAndYear(), is(expectedOutput));
     }
 
     @Test
     public void checkedOutBookShouldNotAppearOnListOfAllBooks() {
-        Library library = new Library(this.listOfBooks);
+        this.library.checkoutBook("Java Basic");
 
-        library.checkoutBook("Java Basic");
-
-        assertThat(library.getListOfBooksName(), is("How to TDD\n"));
+        assertThat(this.library.getListOfBooksName(), is("How to TDD\n"));
     }
 
     @Test
     public void checkedOutBookShouldNotAppearOnListOfAllBooksWithNameAuthorAndYear() {
-        Library library = new Library(this.listOfBooks);
-
         String expectedOutput =
                 "| name                     | author              | Year Published  |\n" +
                 "| -------------------------|---------------------|-----------------|\n" +
                 "| How to TDD               | James               | 2012            |\n";
 
-        library.checkoutBook("Java Basic");
+        this.library.checkoutBook("Java Basic");
 
-        assertThat(library.getListOfAllBooksWithNameAuthorAndYear(), is(expectedOutput));
+        assertThat(this.library.getListOfAllBooksWithNameAuthorAndYear(), is(expectedOutput));
     }
 
     @Test
     public void shouldGetSuccessMessageOnSuccessCheckout() {
-        Library library = new Library(this.listOfBooks);
-
-        String message = library.checkoutBook("Java Basic");
+        String message = this.library.checkoutBook("Java Basic");
 
         assertThat(message, is("Thank you! Enjoy the book"));
     }
 
     @Test
     public void shouldGetUnsuccessfulUnsuccessfulCheckout() {
-        Library library = new Library(this.listOfBooks);
-        library.checkoutBook("Java Basic");
+        this.library.checkoutBook("Java Basic");
 
-        String message = library.checkoutBook("Java Basic");
+        String message = this.library.checkoutBook("Java Basic");
 
         assertThat(message, is("Sorry, that book is not available"));
     }
